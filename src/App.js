bd48de56b2c79540536b41d4229334b695b738b5
Ernesferre <70835371+Ernesferre@users.noreply.github.com>
2021-05-17 17:24:51 -0300

    creando el header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Header from "./components/Header";
 
 
 function App() {
   return (
-   <h1>Noticias</h1>
+   <>
+      <Header
+          titulo="Buscador de Noticias"
+      />
+
+      <div className="container white">
+
+        <h1>Formulario Aqui</h1>
+
+      </div>
+
+
+   </>
   );
 }
 
